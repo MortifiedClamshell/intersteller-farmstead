e451e62ac1a41bff96c428256dd757f34cf22186
MortifiedClamshell <hailie.m.williams@gmail.com>
2024-11-18 11:10:53 -0600

    IDEK

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -1,15 +1,32 @@
 extends Control
 
 var profit = 0
+
+#Wheat variables
 var wheat_amount = 1
+var wheat_timer = 0
+var wheat_timer_D = 1
+
+#Carrot variables
+var carrot_button_cost = 25
 var carrot_amount = 5
 
 signal profit_changed
 
-func _on_wheat_button_button_down() -> void:
-	profit += wheat_amount
-	emit_signal("profit_changed", profit)
+
+#Wheat
+func _on_wheat_timer_timeout() -> void:
+	wheat_timer == wheat_timer_D
+	"value" == wheat_timer
 	
+func _on_wheat_button_button_down() -> void:
+	if wheat_timer == 0:
+		profit += wheat_amount
+		emit_signal("profit_changed", profit)
+	else:
+		pass
+	
+#Carot
 func _on_carrot_button_button_down() -> void:
 	profit += carrot_amount
 	emit_signal("profit_changed", profit)
