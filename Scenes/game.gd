cc4e1b527df9676107e8a08c43d3738cf4e0216b
MortifiedClamshell <hailie.m.williams@gmail.com>
2024-11-20 10:49:54 -0600

    Gave up on timer

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -4,8 +4,6 @@ var profit = 0
 
 #Wheat variables
 var wheat_amount = 1
-var wheat_timer = 0
-var wheat_timer_D = 1
 
 #Carrot variables
 var carrot_button_cost = 25
@@ -13,18 +11,11 @@ var carrot_amount = 5
 
 signal profit_changed
 
-
 #Wheat
-func _on_wheat_timer_timeout() -> void:
-	wheat_timer == wheat_timer_D
-	"value" == wheat_timer
 	
 func _on_wheat_button_button_down() -> void:
-	if wheat_timer == 0:
-		profit += wheat_amount
-		emit_signal("profit_changed", profit)
-	else:
-		pass
+	profit += wheat_amount
+	emit_signal("profit_changed", profit)
 	
 #Carot
 func _on_carrot_button_button_down() -> void:
