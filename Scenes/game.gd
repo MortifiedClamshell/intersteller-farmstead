e6a32e78d2598206d61c7e446ae86a931839022c
MortifiedClamshell <hailie.m.williams@gmail.com>
2024-11-21 11:12:16 -0600

    New Button and unlock system

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -9,6 +9,10 @@ var wheat_amount = 1
 var carrot_button_cost = 25
 var carrot_amount = 5
 
+#Beet variables
+var beet_button_cost = 250
+var beet_amount = 25
+
 signal profit_changed
 
 #Wheat
@@ -18,6 +22,33 @@ func _on_wheat_button_button_down() -> void:
 	emit_signal("profit_changed", profit)
 	
 #Carot
+func _on_carrot_unlock_pressed() -> void:
+	if profit >= carrot_button_cost:
+		profit -= carrot_button_cost
+		emit_signal("profit_changed", profit)
+		$Carrot/CarrotUnlock.disabled = true
+		$Carrot/CarrotUnlock.visible = false
+		$Carrot/CarrotButton.disabled = false
+		$Carrot/CarrotProfit.visible = true
+	else:
+		pass
+
 func _on_carrot_button_button_down() -> void:
 	profit += carrot_amount
+	emit_signal("profit_changed", profit)
+	
+#Beet
+func _on_beet_unlock_pressed() -> void:
+	if profit >= beet_button_cost:
+		profit -= beet_button_cost
+		emit_signal("profit_changed", profit)
+		$Beet/BeetUnlock.disabled = true
+		$Beet/BeetUnlock.visible = false
+		$Beet/BeetButton.disabled = false
+		$Beet/BeetProfit.visible = true
+	else:
+		pass
+
+func _on_beet_button_button_down() -> void:
+	profit += beet_amount
 	emit_signal("profit_changed", profit)
