facba7ec036ae74d7d130e9fd1fdd95bd2408239
MortifiedClamshell <hailie.m.williams@gmail.com>
2024-12-11 11:09:39 -0600

    Updated Graphics

--- Scenes/wheat_bar.gd
+++
@@ -1,7 +0,0 @@
-extends ProgressBar
-
-func _decrease():
-		
-
-func _on_wheat_timer_timeout() -> void:
-	pass # Replace with function body.
--- Scenes/wheat_timer_bar.gd
+++
@@ -1,7 +0,0 @@
-extends Line2D
-
-func _decrease():
-	point[1].x -= 10;
-
-func _on_wheat_timer_timeout() -> void:
-	pass # Replace with function body.
